1e64808838409d8e04f31eb353cd117a9a69ab3a
untodesu <41549137+untodesu@users.noreply.github.com>
2025-04-25 16:30:54 +0500

    Post-push compile errors fix

--- package.windows.x32.bat
+++ package.windows.x32.bat
@@ -1,5 +1,7 @@
 @echo off
-build.windows.x32.bat
+cmake -B build32 -A Win32
+if %errorlevel% neq 0 exit /b %errorlevel%
+cmake --build build32 --config Release --parallel
 if %errorlevel% neq 0 exit /b %errorlevel%
 cpack --config build32/CPackConfig.cmake -G ZIP
 if %errorlevel% neq 0 exit /b %errorlevel%
--- package.windows.x64.bat
+++ package.windows.x64.bat
@@ -1,5 +1,7 @@
 @echo off
-build.windows.x64.bat
+cmake -B build64 -A x64
+if %errorlevel% neq 0 exit /b %errorlevel%
+cmake --build build64 --config Release --parallel
 if %errorlevel% neq 0 exit /b %errorlevel%
 cpack --config build64/CPackConfig.cmake -G ZIP
 if %errorlevel% neq 0 exit /b %errorlevel%
